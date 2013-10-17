a0fa93c2294113b49555a3259d00d4b0367a824d
erikarthur <erikarthur@hotmail.com>
2013-10-17 09:14:34 -0500

    first checkin

---
+++ centralServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace centralServer
+{
+    class Program
+    {
+        List<peer> peers = new List<peer>();
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ centralServer/peer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace centralServer
+{
+    class peer
+    {
+        private string _hostname;
+        private string _ipaddress;
+        private int _port;
+
+        public string hostname
+        {
+            get { return _hostname; }
+            set { _hostname = value; }
+        }
+
+        public string ipaddress
+        {
+            get { return _ipaddress; }
+            set { _ipaddress = value; }
+        }
+
+        public int port
+        {
+            get { return _port; }
+            set { _port = value; }
+        }
+
+    }
+}
