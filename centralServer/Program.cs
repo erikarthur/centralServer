d947b1f126fddf6e70ad07c42148c8b8df73ee70
erikarthur <erikarthur@hotmail.com>
2013-10-17 10:05:04 -0500

    working echo server

--- centralServer/Program.cs
+++ centralServer/Program.cs
@@ -25,19 +25,18 @@ namespace centralServer
 
             // Start listening for client requests.
             server.Start();
-            server.Start();
 
             // Buffer for reading data
             Byte[] bytes = new Byte[256];
             String data = null;
 
-            while(true) 
-              {
+            while (true)
+            {
                 Console.Write("Waiting for a connection... ");
 
                 // Perform a blocking call to accept requests. 
                 // You could also user server.AcceptSocket() here.
-                TcpClient client = server.AcceptTcpClient();            
+                TcpClient client = server.AcceptTcpClient();
                 Console.WriteLine("Connected!");
 
                 data = null;
@@ -48,49 +47,23 @@ namespace centralServer
                 int i;
 
                 // Loop to receive all the data sent by the client. 
-                while((i = stream.Read(bytes, 0, bytes.Length))!=0) 
-                {   
-                  // Translate data bytes to a ASCII string.
-                  data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                  Console.WriteLine("Received: {0}", data);
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
 
-                  // Process the data sent by the client.
-                  data = data.ToUpper();
+                    // Process the data sent by the client.
+                    data = data.ToUpper();
 
-                  byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
 
-                  // Send back a response.
-                  stream.Write(msg, 0, msg.Length);
-                  Console.WriteLine("Sent: {0}", data);            
+                    // Send back a response.
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine("Sent: {0}", data);
                 }
-
-        // Shutdown and end connection
-        client.Close();
+            }
             
-            //Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            ////s.Connect(EPhost);
-
-            ////if (!s.Connected)
-            //// {
-            ////   strRetPage = "Unable to connect to host";
-            ////}
-
-            ////// Use the SelectWrite enumeration to obtain Socket status.  
-            ////if(s.Poll(-1, SelectMode.SelectWrite))
-            ////{
-            ////    Console.WriteLine("This Socket is writable.");
-            ////}
-            ////else if (s.Poll(-1, SelectMode.SelectRead)){
-            ////    Console.WriteLine("This Socket is readable." );
-            ////}
-            ////else if (s.Poll(-1, SelectMode.SelectError)){
-            ////    Console.WriteLine("This Socket has an error.");
-            ////}
-
-            //IPAddress hostIP = (Dns.Resolve(IPAddress.Any.ToString())).AddressList[0];
-            //IPEndPoint ep = new IPEndPoint(hostIP, port);
-            //s.Bind(ep);
         }
     }
 
---
+++ centralServer/peerList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace centralServer
+{
+    class peerList
+    {
+        private string _peerA;
+        private string _peerB;
+
+        public string peerA
+        {
+            get { return _peerA; }
+            set { _peerA = value; }
+        }
+
+        public string peerB
+        {
+            get { return _peerB; }
+            set { _peerB = value; }
+        }
+    }
+}
